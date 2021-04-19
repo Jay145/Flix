6ba0163d78f86b09d5a92b8a9b4fa9bfacca6995
Jay145 <41591950+Jay145@users.noreply.github.com>
2021-04-18 22:27:08 -0700

    assignment 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Jake Yim on 4/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
